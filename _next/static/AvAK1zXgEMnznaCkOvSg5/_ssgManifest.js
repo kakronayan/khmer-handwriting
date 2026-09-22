86d61acacab13ec20e01998c9df00a7d4fad2722
Kakrona Yan <kakrona.yan@wingbank.com.kh>
2026-09-22 17:50:59 +0700

    Deploy static site to GitHub Pages

---
+++ _next/static/AvAK1zXgEMnznaCkOvSg5/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcharacters\u002F[id]","\u002Fcharacters\u002F[id]\u002Fstrokes","\u002Fpractice\u002F[id]","\u002Fpractice\u002F[id]\u002Fresult"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
